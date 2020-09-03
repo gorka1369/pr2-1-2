06ff3a557df0434f8fdde5aaf2160bd710e0ec57
gorka1369 <Tancor@icloud.com>
2020-09-03 17:15:13 +0300

    Добавьте файлы проекта.

---
+++ pr2-1-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace pr2_1_2
+{
+    class Program0
+    {
+        
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher =
+      new FileSystemWatcher(Environment.SystemDirectory);
+            watcher.Filter = "*.ini";
+
+            watcher.IncludeSubdirectories = true;
+
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+
+            watcher.Changed +=
+
+            new FileSystemEventHandler(watcher_Changed);
+            static void watcher_Changed(object sender, FileSystemEventArgs e)
+
+            {
+
+                Console.WriteLine("Changed: {0}", e.FullPath);
+
+            }
+           watcher.EnableRaisingEvents = true;
+        }
+    }
+}
